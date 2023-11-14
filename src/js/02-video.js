78d0492728b40880895608308c4c1302fe72f4c6
Serhii-Kysil <serik.kisel000@gmail.com>
2023-11-14 16:59:15 +0200

    Task2 add

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,21 @@
+import Vimeo from '@vimeo/player';
+const throttle = require('lodash.throttle');
 
+const playerEl = document.querySelector('#vimeo-player');
+const player = new Vimeo(playerEl);
+
+const getSavedTime = () => {
+  return Number(localStorage.getItem('videoplayer-current-time')) || 0;
+}; //будемо повертати час або 0, якщо ще не записано
+
+const updateCurrentTime = throttle(time => {
+  localStorage.setItem('videoplayer-current-time', time);
+}, 1000);
+
+const initialTime = getSavedTime();
+player.setCurrentTime(initialTime);
+
+player.on('timeupdate', data => {
+  const currentTime = data.seconds;
+  updateCurrentTime(currentTime);
+});
